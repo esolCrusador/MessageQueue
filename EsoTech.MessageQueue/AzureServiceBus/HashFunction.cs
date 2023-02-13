19096d02c123d531c1c77bd78d8719e764eeb892
Boris Sotsky <boris.sotsky@gmail.com>
2023-02-13 17:40:32 +0300

    Fixed issue with md5 multithreading.

--- EsoTech.MessageQueue/AzureServiceBus/HashFunction.cs
+++ EsoTech.MessageQueue/AzureServiceBus/HashFunction.cs
@@ -4,27 +4,20 @@ using System.Security.Cryptography;
 
 namespace EsoTech.MessageQueue.AzureServiceBus
 {
-    public class HashFunction: IDisposable
+    public class HashFunction
     {
-        private MD5? _md5;
-
         private readonly ConcurrentDictionary<string, string> _hashes = new ConcurrentDictionary<string, string>();
         public string GetHash(string input) =>
             _hashes.GetOrAdd(input, _ => ComputeMd5Hash(input));
 
         private string ComputeMd5Hash(string input)
         {
-            var md5 = _md5 ??= MD5.Create();
+            using var md5 = MD5.Create();
 
             byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
             byte[] hashBytes = md5.ComputeHash(inputBytes);
 
             return Convert.ToBase64String(hashBytes);
-        }
-
-        public void Dispose()
-        {
-            _md5?.Dispose();
         }
     }
 }
