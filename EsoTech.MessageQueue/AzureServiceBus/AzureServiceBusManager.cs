636525b454c1d1cfc8606572a623efac74382f08
esolCrusador <boris.sotsky@gmail.com>
2024-12-13 19:14:57 +0300

    Optimized initialization

--- EsoTech.MessageQueue/AzureServiceBus/AzureServiceBusManager.cs
+++ EsoTech.MessageQueue/AzureServiceBus/AzureServiceBusManager.cs
@@ -3,9 +3,11 @@ using EsoTech.MessageQueue.Abstractions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EsoTech.MessageQueue.AzureServiceBus
@@ -16,9 +18,11 @@ namespace EsoTech.MessageQueue.AzureServiceBus
         private readonly MessageQueueConfiguration _messageQueueOptions;
         private readonly AzureServiceBusConfiguration _azureOptions;
         private readonly AzureServiceBusNamingConvention _namingConvention;
+        private readonly Task<BusSchema> _initialSchema;
         private readonly ILogger _logger;
 
         private ServiceBusAdministrationClient Client => _client ??= new ServiceBusAdministrationClient(_azureOptions.ConnectionString);
+
 
         public AzureServiceBusManager(IOptions<MessageQueueConfiguration> messageQueueOptions, IOptions<AzureServiceBusConfiguration> azureOptions, AzureServiceBusNamingConvention namingConvention, ILogger<AzureServiceBusManager> logger)
         {
@@ -26,6 +30,52 @@ namespace EsoTech.MessageQueue.AzureServiceBus
             _azureOptions = azureOptions.Value;
             _namingConvention = namingConvention;
             _logger = logger;
+            _initialSchema = GetSchema(default);
+        }
+
+        private async Task<BusSchema> GetSchema(CancellationToken cancellationToken)
+        {
+            var sw = Stopwatch.StartNew();
+            _logger.LogInformation("Retriving schema");
+            var schema = new BusSchema();
+
+            await Task.WhenAll(LoadTopics(schema, cancellationToken), LoadQueues(schema, cancellationToken));
+
+            _logger.LogInformation("Schema loaded. {Milliseconds} milliseconds", sw.ElapsedMilliseconds);
+            return schema;
+        }
+
+        private async Task LoadTopics(BusSchema schema, CancellationToken cancellationToken)
+        {
+            List<Task> initializations = new List<Task>();
+            await foreach (var topic in Client.GetTopicsAsync(cancellationToken))
+            {
+                schema.TryAdd(topic.Name, new ConcurrentDictionary<string, IList<RuleProperties>>());
+                initializations.Add(LoadSubscriptions(schema, topic, cancellationToken));
+            }
+
+            await Task.WhenAll(initializations);
+        }
+
+        private async Task LoadQueues(BusSchema schema, CancellationToken cancellationToken)
+        {
+            await foreach (var queue in Client.GetQueuesAsync(cancellationToken))
+                schema.TryAdd(queue.Name, new ConcurrentDictionary<string, IList<RuleProperties>>());
+        }
+
+        private async Task LoadSubscriptions(BusSchema schema, TopicProperties topicProperties, CancellationToken cancellationToken)
+        {
+            List<Task> initializations = new List<Task>();
+            await foreach (var subscription in Client.GetSubscriptionsAsync(topicProperties.Name, cancellationToken))
+                initializations.Add(LoadRules(schema, subscription, cancellationToken));
+
+            await Task.WhenAll(initializations);
+        }
+
+        private async Task LoadRules(BusSchema schema, SubscriptionProperties subscription, CancellationToken cancellationToken)
+        {
+            IList<RuleProperties> rules = await GetAll(Client.GetRulesAsync(subscription.TopicName, subscription.SubscriptionName, cancellationToken));
+            schema[subscription.TopicName].TryAdd(subscription.SubscriptionName, rules);
         }
 
         public async Task UpdateSubscription(string topicName, string subscriptionName, IEnumerable<Type> messageTypes)
@@ -36,7 +86,10 @@ namespace EsoTech.MessageQueue.AzureServiceBus
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            if (!await Client.SubscriptionExistsAsync(topicName, subscriptionName))
+            IList<RuleProperties>? rules = null;
+            var schema = await _initialSchema;
+            if (!schema.TryGetValue(topicName, out var subscriptions) || !subscriptions.TryGetValue(subscriptionName, out rules))
+            {
                 await Client.CreateSubscriptionAsync(new CreateSubscriptionOptions(topicName, subscriptionName)
                 {
                     MaxDeliveryCount = _azureOptions.MaxDeliveryCount,
@@ -44,26 +97,34 @@ namespace EsoTech.MessageQueue.AzureServiceBus
                     DefaultMessageTimeToLive = _azureOptions.DefaultMessageTimeToLive,
                     DeadLetteringOnMessageExpiration = true
                 });
-            await UpdateRules(topicName, subscriptionName, messageTypes);
+
+                subscriptions = schema.GetOrAdd(topicName, _ => new ConcurrentDictionary<string, IList<RuleProperties>>());
+                subscriptions.TryAdd(subscriptionName, rules = new List<RuleProperties>());
+            }
+            await UpdateRules(topicName, subscriptionName, messageTypes, rules);
 
             stopwatch.Stop();
             _logger.LogInformation($"Subscription {subscriptionName} updated for {stopwatch.ElapsedMilliseconds}ms");
         }
 
-        private async Task UpdateRules(string topicName, string subscriptionName, IEnumerable<Type> messageTypes)
+        private async Task UpdateRules(string topicName, string subscriptionName, IEnumerable<Type> messageTypes, IList<RuleProperties> rules)
         {
             var messageRules = messageTypes.Distinct().ToDictionary(
                 mt => _namingConvention.GetSubscriptionFilterName(mt, 50),
                 mt => _namingConvention.GetSubscriptionFilterValue(mt)
             );
+            rules ??= new List<RuleProperties>();
 
-            var rules = await GetAllRules(topicName, subscriptionName);
-            var rulesToDelete = rules.Where(r => !messageRules.ContainsKey(r.Name));
+            var rulesToDelete = rules.Where(r => !messageRules.ContainsKey(r.Name)).ToList();
             foreach (var rule in rulesToDelete)
+            {
                 await Client.DeleteRuleAsync(topicName, subscriptionName, rule.Name);
+                rules.Remove(rule);
+            }
             var messageTypesToAdd = messageRules.Where(kvp => !rules.Any(r => r.Name == kvp.Key));
             foreach (var (ruleName, messageType) in messageTypesToAdd)
-                await Client.CreateRuleAsync(topicName, subscriptionName, new CreateRuleOptions
+            {
+                var response = await Client.CreateRuleAsync(topicName, subscriptionName, new CreateRuleOptions
                 {
                     Name = ruleName,
                     Filter = new CorrelationRuleFilter
@@ -71,6 +132,8 @@ namespace EsoTech.MessageQueue.AzureServiceBus
                         ApplicationProperties = { ["EsoTechMessageKind"] = messageType }
                     }
                 });
+                rules.Add(response.Value);
+            }
         }
 
         public async Task UpdateTopic(string topicName)
@@ -79,12 +142,17 @@ namespace EsoTech.MessageQueue.AzureServiceBus
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            if (!await Client.TopicExistsAsync(topicName))
+            var schema = await _initialSchema;
+            if (!schema.ContainsKey(topicName))
+            {
                 await Client.CreateTopicAsync(new CreateTopicOptions(topicName)
                 {
                     MaxSizeInMegabytes = _azureOptions.MaxSizeInMB,
                     DefaultMessageTimeToLive = _azureOptions.DefaultMessageTimeToLive,
                 });
+
+                schema.TryAdd(topicName, new ConcurrentDictionary<string, IList<RuleProperties>>());
+            }
 
             stopwatch.Stop();
             _logger.LogInformation($"Topic {topicName} updated for {stopwatch.ElapsedMilliseconds}ms");
@@ -96,20 +164,21 @@ namespace EsoTech.MessageQueue.AzureServiceBus
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            if (!await Client.QueueExistsAsync(queueName))
+            var schema = await _initialSchema;
+            if (!schema.ContainsKey(queueName))
+            {
                 await Client.CreateQueueAsync(new CreateQueueOptions(queueName)
                 {
                     MaxSizeInMegabytes = _azureOptions.MaxSizeInMB,
                     LockDuration = _messageQueueOptions.AckTimeout,
                     DefaultMessageTimeToLive = _azureOptions.DefaultMessageTimeToLive
                 });
+                schema.TryAdd(queueName, new ConcurrentDictionary<string, IList<RuleProperties>>());
+            }
 
             stopwatch.Stop();
             _logger.LogInformation($"Queue {queueName} updated for {stopwatch.ElapsedMilliseconds}ms");
         }
-
-        private Task<IList<RuleProperties>> GetAllRules(string topicName, string subscriptionName) =>
-            GetAll(Client.GetRulesAsync(topicName, subscriptionName));
 
         private static async Task<IList<TEntity>> GetAll<TEntity>(IAsyncEnumerable<TEntity> results)
         {
@@ -127,5 +196,9 @@ namespace EsoTech.MessageQueue.AzureServiceBus
             var topics = await GetAll(Client.GetTopicsAsync());
             await Task.WhenAll(topics.Select(topick => Client.DeleteTopicAsync(topick.Name)));
         }
+    }
+
+    public class BusSchema : ConcurrentDictionary<string, ConcurrentDictionary<string, IList<RuleProperties>>>
+    {
     }
 }
